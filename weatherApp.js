526f680d7c1c6ecebc9c480472c5a07140791e03
Clam119 <Christopher.lam119@gmail.com>
2022-04-05 13:55:37 +0100

    Broke down the url endpoint for readability. Included res.write of the temperature and icon to the client's browser.

--- weatherApp.js
+++ weatherApp.js
@@ -1,23 +1,44 @@
-const express = require('express')
-const https = require('https')
-const app = express()
-const port = 3000
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo, JP&lang=JA&appid=6d691e13a885d8d33c4b2c2bab3612bf&units=metric"
+app.post("/", function (req, res) {
+
+    const query = req.body.cityName;
+    const apiKey = "6d691e13a885d8d33c4b2c2bab3612bf";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+
 
     https.get(url, function (response) {
         console.log(response.statusCode);
 
-        response.on("data", function (data){
-           const weatherData = JSON.parse(data);
-           const temperature = weatherData.main.temp
-           const description = weatherData.weather[0].description
-           console.log("今日は" + temperature + " 零度で" + description + "です");
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temperature = weatherData.main.temp
+            const description = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const iconImageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png" //String concactenation to create iconImageUrl to callback.
+            res.write("<h1>The temperature in " + query + " is " + temperature + " degrees celcius.</h1>");
+            res.write("The weather is currently " + description);
+            res.write("<img src=" + iconImageUrl + ">"); //Img's self-closing tag, so concactenate img code with iconImageUrl.
+            res.send();
         })
     })
 })
+
+
+
 
 app.listen(port, function () {
     console.log('Server is running on port 3000');
